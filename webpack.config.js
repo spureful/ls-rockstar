8003a31497a207971965345d1248b9273947ffc4
Ann <rw1zz@yandex.ru>
2020-06-13 15:31:38 +0300

    full path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
 	const isProductionBuild = argv.mode === "production";
-	const publicPath = '/ls-rockstar/';
+	const publicPath = 'https://spureful.github.io/ls-rockstar/';
 
 	const pcss = {
 		test: /\.(p|post|)css$/,
